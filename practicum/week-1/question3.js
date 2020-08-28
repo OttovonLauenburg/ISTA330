c35f6483b673203c38fd838cb2ed97ad77d85204
OttovonLauenburg <xiaoxinyi1688@163.com>
2020-08-27 20:17:00 -0700

    numberOfOrderedPairs codes

--- practicum/week-1/question3.js
+++ practicum/week-1/question3.js
@@ -9,5 +9,15 @@ There are 5 ordered pairs: (4, 6), (4, 9), (2, 6), (2, 9)
 */
 
 var numberOfOrderedPairs = function(input) {
-
+  var len = input.length;
+  var num = 0;
+  for (i = 0; i <= len; i ++) {
+    for (j = i ; j <len; j ++) {
+      if (input[j] > input[i]) {
+        num += 1
+      }
+    }
+  }
+  
+  return num;
 };
