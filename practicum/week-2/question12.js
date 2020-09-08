19b7546e04431604d6eb270e92c848ef5eeb7ccc
OttovonLauenburg <xiaoxinyi1688@163.com>
2020-09-08 15:23:38 -0700

    Q12 Update

--- practicum/week-2/question12.js
+++ practicum/week-2/question12.js
@@ -9,5 +9,9 @@ Given n, calculate F(n).
 */
 
 var F = function(n) {
-
+  var fibonacci = [0,1];
+  for (let i = 2; i <= n; i ++){
+    fibonacci.push(fibonacci[i - 1] + fibonacci[i - 2]);
+  }
+  return fibonacci[fibonacci.length - 1];
 };
