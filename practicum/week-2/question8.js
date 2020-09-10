2d9e63f1c2392f4d09c5656af97b16d29e81d83f
OttovonLauenburg <xiaoxinyi1688@163.com>
2020-09-10 05:26:07 -0700

    Update

--- practicum/week-2/question8.js
+++ practicum/week-2/question8.js
@@ -13,5 +13,37 @@ output: 3
 */
 
 var d_integer = function(input) {
+   var stat = {};
+   for (let x of input) {
+    if (x in stat === false){
+      stat[x] = 0;
+     }
+     stat[x] += 1;
+    }
+  
+    var d_arr = [];
+    for (let y in stat){
+      if (Number(y) === stat[y]){
+        d_arr.push(stat[y]);
+      }
+    }
+  
+    var largest = function(arr){
+      let big_n = 0;
+      for (let z of arr){
+        if (z > big_n){
+          big_n = z;
+        }
+      }
+     return big_n;
+    };
 
+  if (d_arr.length === 0){
+    return -1
+  } else if (d_arr.length === 1){
+    return d_arr[0];
+  } else {
+    return Number(largest(d_arr));
+  }
+  
 };
--- practicum/week-2/question9.js
+++ practicum/week-2/question9.js
@@ -13,5 +13,21 @@ so there are two groups with the largest size.
 */
 
 var largestGroupsCount = function(n) {
-
+  var x = 10;
+  var digitsum = [];
+  while (x <= n){
+    let stringnum = String(x);
+    stringnum.split("");
+    y = Number(stringnum[0]) + Number(stringnum[1]);
+    digitsum.push(y);
+    x += 1;
+  }
+  
+  var count = 0;
+  for (let z of digitsum){
+    if (z >= 1 && z <= 9){
+      count += 1;
+    }
+  }
+  return count;
 };
